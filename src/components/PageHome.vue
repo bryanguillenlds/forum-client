fcfacf4d4b7189a626d760fe6c9f48f7e9a1e1d4
Bryan Guillen <bryanguillenlds@gmail.com>
2024-04-26 23:27:53 -0400

    Removed references to HelloWorld component. Created new home page component and started utilizing data to display user name and post-text for the post of each thread.

---
+++ src/components/PageHome.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>Page Home</div>
+  <div v-for="thread in threads" :key="thread.id">
+    <h2>{{ thread.title }}</h2>
+    <div v-for="postId in thread.posts" :key="postId">
+      <p>{{ getUserById(getPostById(postId).userId).name }}</p>
+      <p>{{ getPostById(postId).text }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import sourceData from '@/data.json'
+
+export default {
+  name: 'PageHome',
+
+  data () {
+    return {
+      threads: sourceData.threads,
+      posts: sourceData.posts,
+      users: sourceData.users
+    }
+  },
+
+  methods: {
+    getPostById (postId) {
+      return this.posts.find(post => post.id === postId)
+    },
+
+    getUserById (userId) {
+      return this.users.find(user => user.id === userId)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
