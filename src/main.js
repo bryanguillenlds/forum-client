704e3c022d3774efba8185ffe7562373f0a92ede
Bryan Guillen <bryanguillenlds@gmail.com>
2024-04-29 18:24:54 -0400

    Fixed dependency issues to install vue router. Initialized router and added routes for home and single thread component. Created single thread component. Using router to pass it the id of the thread and show the correct one. Added router-view to root component.

--- src/main.js
+++ src/main.js
@@ -1,4 +1,33 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import { createRouter, createWebHistory } from 'vue-router'
+
+import PageHome from '@/components/PageHome.vue'
+import PageThreadShow from '@/components/PageThreadShow.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: PageHome
+  },
+  {
+    path: '/thread/:id',
+    name: 'ThreadShow',
+    component: PageThreadShow,
+    props: true
+  }
+
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+
+const app = createApp(App)
+
+app.use(router)
+
+app.mount('#app')
