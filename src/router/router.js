bf8b15c199b81f54449d755325c25d9ef05f51b3
Bryan Guillen <bryanguillenlds@gmail.com>
2024-05-11 21:22:18 -0400

    Added route guard in case of nonexisting threads. Wrong URL will be preserved so user has a chance to fix it.

--- src/router/router.js
+++ src/router/router.js
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import sourceData from '@/data.json'
 
 import PageHome from '@/components/PageHome.vue'
 import PageThreadShow from '@/components/PageThreadShow.vue'
@@ -14,7 +15,23 @@ const routes = [
     path: '/thread/:id',
     name: 'ThreadShow',
     component: PageThreadShow,
-    props: true
+    props: true,
+
+    beforeEnter (to, from, next) {
+      // Check if thread id exists, redirect if doesn't
+      const threadExists = sourceData.threads.find(thread => thread.id === to.params.id)
+
+      if (threadExists) {
+        return next()
+      } else {
+        next({
+          name: 'NotFound',
+          params: { pathMatch: to.path.substring(1).split('/') },
+          query: to.query,
+          hash: to.hash
+        })
+      }
+    }
   },
   {
     path: '/:pathMatch(.*)*',
