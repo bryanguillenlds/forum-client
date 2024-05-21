6e630b3b98017655142d0c5ad9e965bac69763d6
Bryan Guillen <bryanguillenlds@gmail.com>
2024-05-20 21:27:56 -0400

    Added a component to handle displaying a list of posts

---
+++ src/components/PostList.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="post-list">
+    <div
+      v-for="post in posts"
+      :key="post.id"
+      class="post"
+    >
+      <div class="user-info">
+        <a href="#" class="user-name">{{ getUserById(post.userId).name }}</a>
+
+        <a href="#">
+          <img class="avatar-large" :src="getUserById(post.userId).avatar" alt="">
+        </a>
+
+        <p class="desktop-only text-small">107 posts</p>
+      </div>
+
+      <div class="post-content">
+        <div>
+          <p>
+            {{ post.text }}
+          </p>
+        </div>
+      </div>
+
+      <div class="post-date text-faded">
+        {{ post.publishedAt }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import sourceData from '@/data.json'
+
+export default {
+  name: 'PostList',
+
+  props: {
+    posts: {
+      type: Object,
+      required: true
+    }
+  },
+
+  data () {
+    return {
+      users: sourceData.users
+    }
+  },
+
+  methods: {
+    /**
+     * Gets user by id.
+     *
+     * @param {String} userId - The user ID.
+     * @returns {Object} - The user object that was found.
+     **/
+    getUserById (userId) {
+      return this.users.find(user => user.id === userId)
+    }
+  }
+}
+
+</script>
+
+<style>
+
+</style>
--- src/pages/ThreadShow.vue
+++ src/pages/ThreadShow.vue
@@ -2,43 +2,20 @@
   <div class="col-large push-top">
     <h1>{{ thread.title }}</h1>
 
-    <div class="post-list">
-      <div
-        v-for="postId in thread.posts"
-        :key="postId"
-        class="post"
-      >
-        <div class="user-info">
-          <a href="#" class="user-name">{{ getUserById(getPostById(postId).userId).name }}</a>
-
-          <a href="#">
-            <img class="avatar-large" :src="getUserById(getPostById(postId).userId).avatar" alt="">
-          </a>
-
-          <p class="desktop-only text-small">107 posts</p>
-        </div>
-
-        <div class="post-content">
-          <div>
-            <p>
-              {{ getPostById(postId).text }}
-            </p>
-          </div>
-        </div>
-
-        <div class="post-date text-faded">
-          {{ getPostById(postId).publishedAt }}
-        </div>
-      </div>
-    </div>
+    <PostList :posts="threadPosts"/>
   </div>
 </template>
 
 <script>
 import sourceData from '@/data.json'
+import PostList from '@/components/PostList.vue'
 
 export default {
-  name: 'PageThreadShow',
+  name: 'ThreadShow',
+
+  components: {
+    PostList
+  },
 
   props: {
     id: {
@@ -50,8 +27,7 @@ export default {
   data () {
     return {
       threads: sourceData.threads,
-      posts: sourceData.posts,
-      users: sourceData.users
+      posts: sourceData.posts
     }
   },
 
@@ -63,28 +39,10 @@ export default {
      */
     thread () {
       return this.threads.find(thread => thread.id === this.id)
-    }
-  },
-
-  methods: {
-    /**
-     * Gets post by id.
-     *
-     * @param {String} postId - The post ID.
-     * @returns {Object} - The post object that was found.
-     **/
-    getPostById (postId) {
-      return this.posts.find(post => post.id === postId)
     },
 
-    /**
-     * Gets user by id.
-     *
-     * @param {String} userId - The user ID.
-     * @returns {Object} - The user object that was found.
-     **/
-    getUserById (userId) {
-      return this.users.find(user => user.id === userId)
+    threadPosts () {
+      return this.posts.filter(post => post.threadId === this.id)
     }
   }
 }
