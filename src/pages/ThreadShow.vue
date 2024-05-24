e2674e6963766ab56119b23cd25feafc856bfa9c
Bryan Guillen <bryanguillenlds@gmail.com>
2024-05-23 20:52:03 -0400

    Added method to add a post to the array of posts and to push a post id to a specific array of posts that belong to a thread.

--- src/pages/ThreadShow.vue
+++ src/pages/ThreadShow.vue
@@ -3,6 +3,17 @@
     <h1>{{ thread.title }}</h1>
 
     <PostList :posts="threadPosts"/>
+
+    <div class="col-full">
+      <form @submit.prevent="addPost">
+        <div class="form-group">
+          <textarea v-model="newPostText" cols="30" rows="10" class="form-input"/>
+        </div>
+        <div class="form-actions">
+          <button class="btn-blue">Submit post</button>
+        </div>
+      </form>
+    </div>
   </div>
 </template>
 
@@ -27,7 +38,8 @@ export default {
   data () {
     return {
       threads: sourceData.threads,
-      posts: sourceData.posts
+      posts: sourceData.posts,
+      newPostText: ''
     }
   },
 
@@ -43,6 +55,25 @@ export default {
 
     threadPosts () {
       return this.posts.filter(post => post.threadId === this.id)
+    }
+  },
+
+  methods: {
+    addPost () {
+      const postId = 'ggqq' + Math.random()
+
+      const post = {
+        id: postId,
+        text: this.newPostText,
+        publishedAt: Math.floor(Date.now() / 1000),
+        threadId: this.id,
+        userId: 'rpbB8C6ifrYmNDufMERWfQUoa202'
+      }
+
+      this.posts.push(post) // Add entire post to arr of posts
+      this.thread.posts.push(postId) // Add post id to array of posts pertaining to the specific thread
+
+      this.newPostText = ''
     }
   }
 }
