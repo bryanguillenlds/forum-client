a3767b83d801b22230218b2bc7295ea8b53903af
Bryan Guillen <bryanguillenlds@gmail.com>
2024-06-03 21:20:58 -0400

    Created a component for a list of forums. Included it in home page instead of threads.

---
+++ src/components/ForumList.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="col-full">
+    <div class="forum-list">
+
+      <h2 class="list-title">
+        <a href="#">Forums</a>
+      </h2>
+
+      <div class="forum-listing" v-for="forum in forums" :key="forum.id">
+        <div class="forum-details">
+          <router-link
+            :to="{ name: 'Forum', params: {id: forum.id }}"
+            class="text-xlarge"
+          >
+            {{ forum.name }}
+          </router-link>
+          <p>{{ forum.description }}</p>
+        </div>
+
+        <div class="threads-count">
+          <p>
+            <span class="count">{{ forum.threads?.length }}</span>
+            {{ forumThreadsWord(forum) }}
+          </p>
+        </div>
+
+        <div class="last-thread"></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ForumList',
+
+  props: {
+    forums: {
+      required: true,
+      type: Array
+    }
+  },
+
+  /**
+   * Returns the appropriate word ('thread', 'threads', or 'no threads') based on the number of threads in the given forum.
+   *
+   * @param {Object} forum - The forum object containing the threads array.
+   * @returns {string} The appropriate word based on the number of threads.
+   */
+  methods: {
+    forumThreadsWord (forum) {
+      if (forum.threads?.length) {
+        return forum.threads.length > 1 ? 'threads' : 'thread'
+      } else {
+        return 'no threads'
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,43 +1,19 @@
 <template>
   <h1>Welcome to the Forum</h1>
-  <ThreadList :threads="threads" />
+  <ForumList :forums="forums"/>
 </template>
 
 <script>
 import sourceData from '@/data.json'
-import ThreadList from '@/components/ThreadList.vue'
+import ForumList from '@/components/ForumList'
 
 export default {
   name: 'PageHome',
-  components: { ThreadList },
+  components: { ForumList },
 
   data () {
     return {
-      threads: sourceData.threads,
-      posts: sourceData.posts,
-      users: sourceData.users
-    }
-  },
-
-  methods: {
-    /**
-     * Gets post by id.
-     *
-     * @param {String} postId - The post ID.
-     * @returns {Object} - The post object that was found.
-     **/
-    getPostById (postId) {
-      return this.posts.find(post => post.id === postId)
-    },
-
-    /**
-     * Gets user by id.
-     *
-     * @param {String} userId - The user ID.
-     * @returns {Object} - The user object that was found.
-     **/
-    getUserById (userId) {
-      return this.users.find(user => user.id === userId)
+      forums: sourceData.forums
     }
   }
 }
